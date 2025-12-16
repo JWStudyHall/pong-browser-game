edc90dfde3353bac84117b04ad90cc517df91d49
JEStudyhall <jaieelizabeth@gmail.com>
2025-12-16 16:42:25 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const canvas = document.getElementById("myCanvas");
+const ctx = canvas.getContext("2d");
+const runButton = document.getElementById("runButton");
+const ballRadius = 20;
+const paddleWidth = 75;
+const paddleHeight = 10;
+
+let paddleX = (canvas.width - paddleWidth) / 2;
+let x = canvas.width / 2;
+let y = canvas.height - 30;
+let dx = 2;
+let dy = -2;
+let rightPressed = false;
+let leftPressed = false;
+
+function drawPaddle() {
+  ctx.beginPath();
+  ctx.rect(paddleX, canvas.height - paddleHeight, paddleWidth, paddleHeight);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function drawBall() {
+  ctx.beginPath();
+  ctx.arc(x, y, ballRadius, 0, Math.PI * 9);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+}
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  drawBall();
+  if (x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
+    dx = -dx;
+  }
+  if (y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    dy = -dy;
+  }
+  if (rightPressed) {
+    paddleX = Math.min(paddleX + 7, canvas.width - paddleWidth);
+  } else if (leftPressed) {
+    paddleX = Math.max(paddleX - 7, 0);
+  }
+
+  x += dx;
+  y += dy;
+  drawPaddle();
+}
+
+function startGame() {
+  //   drawPaddle();
+  setInterval(draw, 10);
+  document.addEventListener("keydown", keyDownHandler);
+  document.addEventListener("keyup", keyUpHandler);
+  function keyDownHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = true;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = true;
+    }
+  }
+
+  function keyUpHandler(e) {
+    if (e.key === "Right" || e.key === "ArrowRight") {
+      rightPressed = false;
+    } else if (e.key === "Left" || e.key === "ArrowLeft") {
+      leftPressed = false;
+    }
+  }
+}
+
+runButton.addEventListener("click", () => {
+  startGame();
+  runButton.disabled = true;
+});
